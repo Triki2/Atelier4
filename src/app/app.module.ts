e4193a66b40d2c0f1095c29cc9751477525e15af
triki12 <dalitriki53@gmail.com>
2023-03-31 17:38:42 +0100

    Atelier4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { TodoListComponent} from './todo-list/todo-list.component';
+import {UsersComponent} from './users/users.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule, 
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/calcul-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculServiceService {
+
+  constructor() { }
+  
+  getNumberOf(list:any[], critiria:string, value:any):number{
+   return  list.filter(e=>e[critiria]==value).length
+
+  }
+}
+
+
